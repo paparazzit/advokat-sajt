71184453f6351752f138de741117db6b06abcaee
unknown <paparazzit@gmail.com>
2020-11-17 16:05:48 +0100

    slider

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
+    dots: false,
     nav: true,
+
+    autoWidth: true,
     responsive: {
       0: {
         items: 1,
@@ -14,5 +17,13 @@ $(document).ready(function () {
         items: 4,
       },
     },
+  });
+  owl.on("mousewheel", ".owl-stage", function (e) {
+    if (e.deltaY > 0) {
+      owl.trigger("next.owl");
+    } else {
+      owl.trigger("prev.owl");
+    }
+    e.preventDefault();
   });
 });
